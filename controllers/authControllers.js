a4160a7ef55b8af5d3ff6fe0cdec08106d7a36fb
niloysaha06 <niloysaha895@gmail.com>
2023-05-23 17:14:58 +0600

    create register(server)

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,31 @@
-exports.registerContoller = async () => {
+const userModel = require('../models/userModel')
+const errorResponse = require('../utils/errorResponse')
 
+
+//JWT TOKEN
+exports.sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken(res)
+    res.status(statusCode).json({
+        success : true,
+        token
+    })
+}
+
+//Register
+exports.registerContoller = async (req, res, next) => {
+    try{
+       const {username, email, password} = req.body
+       //exixiting user
+       const exisitingEmail = await userModel.finfOne({email})
+       if(exisitingEmail){
+        return next(new errorResponse('Email is already register', 500))
+       }
+       const user = await userModel.create({username, email, password})
+       sendToken(user, 201, res)
+    } catch(error) {
+        console.log(error)
+        next(error)
+    }
 }
 
 exports.loginController = async () => {
@@ -8,5 +34,5 @@ exports.loginController = async () => {
 
 exports.logoutController = async () => {
 
-    
+
 }
